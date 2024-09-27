8a1934bc2bfa267ef659a59c057d1b19f7203bc8
Sara Blackburn <Sara.blackburn31@gmail.com>
2024-09-27 18:14:44 +0000

    added function for location descriptions and tested start location

--- run.py
+++ run.py
@@ -14,6 +14,17 @@ class Player:
         print(f"\n{self.name} - HP: {self.hp}")
         print("Inventory:", ", ".join(self.inventory) if self.inventory else "Empty")
     
+def describe_location(player):
+    """
+    Function to display the current location
+    """
+    locations = {
+        "start": "You are at the entrance of a dark forest. There is a path you can see ahead. It leads into the forest.",
+        "forest": "You have entered the forest. It's almost too dark to see. You can barely make out an apple tree with shiny red apples on it. It's quiet... too quiet.",
+        "cave": "You find a cave. It is darker than the forest (somehow) and cold.. You hear growling inside.. It's getting louder."
+    }
+    print(locations[player.location])
+
 
 def main():
     """
@@ -27,6 +38,6 @@ def main():
         name = input("Enter your character's name:\n").strip()
     
     player = Player(name)
-    player.show_status()
+    describe_location(player)
 
 main()
