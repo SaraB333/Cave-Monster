272c1a59abf87d4d1ea53bc1390bb4dc049178b1
Sara Blackburn <Sara.blackburn31@gmail.com>
2024-09-27 17:55:08 +0000

    added function for running the game and name input for user

--- run.py
+++ run.py
@@ -1,8 +1,25 @@
 import random
 
 class Player:
+    """
+    Player class to handle player attributes and actions
+    """
     def __init__(self, name):
         self.name = name
         self.hp = 100
         self.inventory = []
         self.location = "start"
+    
+
+def main():
+    """
+    Name input for user
+    Run all program functions
+    """
+    print("Welcome Adventurer! What is your name?")
+    name = input("Enter your character's name:\n").strip()
+    while not name:
+        print("Name cannot be blank.")
+        name = input("Enter your character's name:\n").strip()
+
+main()
