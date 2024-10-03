5b7732c0919967c22d410bd4cb49ed3006d0583c
Sara Blackburn <Sara.blackburn31@gmail.com>
2024-10-03 18:34:44 +0000

    fixed all errors from linter

--- run.py
+++ run.py
@@ -60,16 +60,20 @@ def describe_location(player):
     Function to display the current location and available actions
     """
     locations = {
-        "start": "You are at the entrance of a dark forest. There is a path\
-            you can see ahead.\nIt leads into the forest.",
-        "forest": "It's almost too dark to see.\nYou can barely make out an\
-            apple tree with shiny red apples on it.\nIt's quiet... too quiet.",
-        "cave": "It's even darker than the forest (somehow).. You hear growling\
-            inside..\nIt's getting louder."
+        "start": ("You are at the entrance of a dark forest. There is a path "
+                  + "you can see ahead.\nIt leads into the forest."),
+        "forest": ("It's almost too dark to see.\nYou can barely make out an"
+                   + "apple tree with shiny red apples on it." +
+                   "\nIt's quiet... too quiet."),
+        "cave": ("It's even darker than the forest (somehow).." +
+                 "You hear growling inside..\nIt's getting louder.")
     }
-    print(locations[player.location]) # Tells the user where they are in the game
-    time.sleep(1.5)  # Adds a time delay
-    available_actions(player) # Tells the player what they can do
+    print(locations[player.location])
+    # Tells the user where they are in the game
+    time.sleep(1.5)
+    # Adds a time delay
+    available_actions(player)
+    # Tells the player what they can do
 
 
 def available_actions(player):
@@ -89,8 +93,10 @@ def available_actions(player):
         print("2. Run back to the forest...")
 
     print("\nPlease input the NUMBER of the action you wish to take.")
-    action = input("Choose an action:\n") # Input for player to use for actions
-    handle_action(player, action) # Actions the users input above
+    action = input("Choose an action:\n")
+    # Input for player to use for actions
+    handle_action(player, action)
+    # Actions the users input above
 
 
 def handle_action(player, action):
@@ -99,96 +105,118 @@ def handle_action(player, action):
     """
     if player.location == "start":
         if action == "1":
-            print("\nYou walk into the forest. The light of day fades the more you walk...")
+            print("\nYou walk into the forest.")
+            print("The light of day fades the more you walk...")
             player.location = "forest"
         elif action == "2":
             player.show_status()
         else:
-            print("\nInvalid action. Please ensure you input a number for an action that is avalable.\
-            \nOnly a number will be accepted.\n")
+            print("\nInvalid action. Please ensure you input a number" +
+                  "for an action that is avalable.")
+            print("Only a number will be accepted.\n")
     elif player.location == "forest":
         if action == "1":
-            print("\nYou venture deeper and discover a mysterious cave... of course you go in!!")
+            print("\nYou venture deeper and discover a mysterious cave...")
+            print("Of course you go in!!")
             player.location = "cave"
         elif action == "2":
-            apple_heal = random.randint(1, 5) # Amount apple will heal player by
+            apple_heal = random.randint(1, 5)
+            # Amount apple will heal player by
             player.heal_player(apple_heal)
-            if player.hp >= 100: # Ensures the player never goes above full health
+            if player.hp >= 100:
+                # Ensures the player never goes above full health
                 print("\n")
                 print(f"{Fore.GREEN}You are at full health.{Style.RESET_ALL}")
-                    # Adds colour to the text for a better player experience
+                # Adds colour to the text for a better player experience
                 print("\n")
             else:
                 print("\n")
-                print(f"The apple healed you by {Fore.GREEN}{apple_heal}{Style.RESET_ALL}")
-                    # Tells the user how much the player is healed
+                print(f"The apple healed you by {Fore.GREEN}{apple_heal}"
+                      + "{Style.RESET_ALL}")
+                # Tells the user how much the player is healed
                 print("\n")
         elif action == "3":
             player.show_status()
         elif action == "4":
-            print("\nYou head back to where you started.. nothing has changed.")
+            print("\nYou head back to where you started.." +
+                  "nothing has changed.")
             player.location = "start"
         else:
-            print("\nInvalid action. Please ensure you input a number for an action that is avalable.\
-            \nOnly a number will be accepted.\n")
+            print("\nInvalid action. Please ensure you input a number" +
+                  "for an action that is avalable.")
+            print("Only a number will be accepted.\n")
     elif player.location == "cave":
         if action == "1":
             fight_monster(player)
         elif action == "2":
-            print("\nYou run back to the forest covered in sweat and other bodily fluids...\
-            \nHow did they get there?! You're not scared...")
+            print("\nYou run back to the forest covered in sweat" +
+                  "and other bodily fluids...\nHow did they get there?! "
+                  + "You're not scared...")
             player.location = "forest"
         else:
-            print("\nInvalid action. Please ensure you input a number for an action that is avalable.\
-            \nOnly a number will be accepted.\n")
+            print("\nInvalid action. Please ensure you input a number" +
+                  "for an action that is avalable.")
+            print("Only a number will be accepted.\n")
 
     if player.still_alive():
-        describe_location(player) # Describes the location only if the player is still alive
+        describe_location(player)
+        # Describes the location only if the player is still alive
     else:
         print("\n")
         print(f"{Fore.RED}{player.name} has perished...{Style.RESET_ALL}")
-    
+
 
 def fight_monster(player):
     """
-    Function to handle fighting the monster in the cave. 
+    Function to handle fighting the monster in the cave.
     Options for attack or run
     Handles damage from monster and to monster
     Handles damage to player
     """
-    monster_hp = 50 # Starting hp of Monster
-    print("\nA feral beast emerges from the shadows.\
-    There's no chance of taming this thing... KILL IT!!!\n")
+    monster_hp = 50
+    # Starting hp of Monster
+    print("\nA feral beast emerges from the shadows." +
+          "There's no chance of taming this thing... KILL IT!!!\n")
     while monster_hp > 0 and player.still_alive():
         print("Please input either 'a' or 'r' only.")
         action = input("Do you want to (a)ttack or (r)un?\n").lower()
-            # Ensures the input will be accepted if the user inputs a capital letter
+        # Ensures the input will be accepted if the user inputs uppercase
         if action == "a":
-            damage = random.randint(5, 15) # Amount player will damage monster
+            damage = random.randint(5, 15)
+            # Amount player will damage monster
             monster_hp -= damage
             print("\n")
-            print(f"You dealt {Fore.RED}{damage}{Style.RESET_ALL} damage to the monster.. Will it be enough??")
-            if monster_hp > 0: # If monster is alive, it will attack the player
-                monster_attack = random.randint(10, 25) # Amount monster will damage player
+            print(f"You dealt {Fore.RED}{damage}{Style.RESET_ALL} " +
+                  "damage to the monster.. Will it be enough??")
+            if monster_hp > 0:
+                # If monster is alive, it will attack the player
+                monster_attack = random.randint(10, 25)
+                # Amount monster will damage player
                 player.take_damage(monster_attack)
-                print(f"The monster dealt {Fore.RED}{monster_attack}{Style.RESET_ALL} damage to you.")
+                print("The monster dealt" +
+                      f" {Fore.RED}{monster_attack}{Style.RESET_ALL}"
+                      + " damage to you.")
                 print("\n")
             else:
-                print("\nYou defeated the monster!! You loot it's bloody corpse.")
-                player.add_item("Monster's treasure") # Adds monsters loot to players inventory
-                print(f"Congratulations, {player.name}!\
-                You have defeated the monster and won the game!\
-                You bring the treasure back home and the town builds\
-                a statue in {player.name}'s honour! Wow!")
+                print("\nYou defeated the monster!! " +
+                      "You loot it's bloody corpse.")
+                player.add_item("Monster's treasure")
+                # Adds monsters loot to players inventory
+                print(f"Congratulations, {player.name}!" +
+                      " You have defeated the monster and won the game!" +
+                      " You bring the treasure back home and the town builds" +
+                      " a statue in {player.name}'s honour! Wow!")
                 player.show_status()
-                exit()  # Ends the game after defeating the monster
+                exit()
+                # Ends the game after defeating the monster
         elif action == "r":
-            print("\nYou escape back to the forest. Those apples look extra juicy...")
+            print("\nYou escape back to the forest. " +
+                  "Those apples look extra juicy...")
             player.location = "forest"
             return
         else:
             print("Invalid action. Please input either 'a' or 'r' only.")
-    
+
     if not player.still_alive():
         print("You have been slain by the monster..")
         time.sleep(1.5)
@@ -207,7 +235,7 @@ def main():
     while not name:
         print("Name cannot be blank.")
         name = input("Enter your character's name:\n").strip()
-    
+
     player = Player(name)
     describe_location(player)
 
